50cf95eaaa05a2a8aa4dcf2c7f647cdb5f0cbdad
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-09-23 14:33:04 +0100

    feat: add announcement post

--- orepper-landing/app/page.tsx
+++ orepper-landing/app/page.tsx
@@ -1,10 +1,28 @@
 import Installation from "@/components/code/installation";
 import Spotlight from "@/components/ui/spotlight";
 import TypingComponent from "@/components/ui/typing";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { LinkedInLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="mt-10 ml-6 md:ml-4">
+      <div className="flex justify-center items-center cursor-pointer">
+        <div className=" md:w-[40%]  bg-blue-400 p-1 rounded-2xl place-items-center text-white transition hover:bg-blue-500">
+          <Link
+            className=" flex items-center  gap-2"
+            href="https://www.linkedin.com/posts/timothy-ogbemudia-5961661a5_react-opensource-webdevelopment-activity-7111339755795038208-q0HV?utm_source=share&utm_medium=member_desktop"
+            target="_blank"
+          >
+            <LinkedInLogoIcon className="ml-2" />
+            <span className="text-base">
+              Introducing ore-pper annoucement post
+            </span>
+            <ArrowRightIcon width={15} height={15} />
+          </Link>
+        </div>
+      </div>
       <h1
         className={
           " text-3xl flex flex-col text-center md:text-5xl  w-full p-2  mb-2"
